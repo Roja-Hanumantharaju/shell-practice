d1fc7628e333cd000ab30d790bdee8b26112e7fd
ROJA GOWDA <roja95430@gmail.com>
2025-05-21 15:08:00 +0530

    hello world started

---
+++ 2-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Trump: hey musk, how are you?"
+echo "Musk:: Hello trump, I am fine. HOw are you doing"
+echo "Trump: I am fine too. what's up?"
+echo "Musk: Nothing,just going to mars now will you come"
+echo "Trump: Sorry, you carry on! I will come once you come back"
